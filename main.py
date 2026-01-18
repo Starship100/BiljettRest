bb2a3b81c4b0c25a863cedb63ef20449821099b3
Starship100 <jimmy.widlund@outlook.com>
2026-01-18 14:49:28 +0100

    BiljettRest

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ ticket_price = 100
 account = 200
 sum = account - ticket_price
 print("Det blir " + str(account - ticket_price) + " kronor över.")
-Split = int(sum / 2)
-print("Varje person får " + str(Split) + " kronor.")
+split = int(sum / 2)
+print("Varje person får " + str(split) + " kronor.")
 
 # Slut
